b589757b8d461419806b185cd63dc6afbabb0213
Rafael <rafaelperez@airmail.cc>
2015-05-08 16:25:38 -0600

    Update remover_acentos.py

--- remover_acentos.py
+++ remover_acentos.py
@@ -1,6 +1,6 @@
-lista= []
-    for palabra2 in linea. split ():
-        palabra3=[]
-        for palabra2 in linea.split():
-            palabra3.append(palabra2.strip("áéióúñ"))
-        print(" ".join(palabra3))
+def removerAcentos(lista):
+        for palabra2 in linea. split ():
+            palabra3=[]
+            for palabra2 in linea.split():
+                palabra3.append(palabra2.strip("áéióúñ"))
+            return " ".join(palabra3)
